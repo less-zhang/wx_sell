35f20e8430d1e8fb10151dcb51055e4cdf9f2fad
xmcc <1948843513@qq.com>
2019-04-15 14:27:53 +0800

    第一次提交：项目环境搭建

---
+++ src/main/java/com/xmcc/WxSellApplication.java
@@ -0,0 +1,19 @@
+package com.xmcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxSellApplication.class, args);
+    }
+
+}
+
+
+/**
+ * git remote add origin https://github.com/less-zhang/wx_sell.git
+ * git push -u origin master
+ */
